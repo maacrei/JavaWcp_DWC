09c0d46f6f5f068407e1b2ac04252860e83ddc51
神原 茉里 <maacrei5723@gmail.com>
2022-05-19 17:56:43 +0900

    基礎編５章まで

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "ねこ";
+		System.out.println(str);
+		str = "いぬ";
+		System.out.println(str);
+		final int LUCKY = 7;
+		System.out.println(LUCKY);
+	}
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 123456L;
+		System.out.println(l);
+		double d = 1.1;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Float f = 2.2222f;
+		System.out.println(f);
+		Integer i = 3;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+}
